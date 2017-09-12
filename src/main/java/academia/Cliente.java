867cdd36b42cd61b865e665da3eef7686b48e07d
Renata Argerique <renatapva@gmail.com>
2017-09-11 23:00:54 -0300

    Initial service

--- src/main/java/academia/Cliente.java
+++ src/main/java/academia/Cliente.java
@@ -1,8 +1,17 @@
 package academia;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
 	private String nome;
 	
 	public long getId() {

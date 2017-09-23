3e22f61f1b47d622b6ace973155e4f0e78decb58
Ayres Flesch <ayresabf@gmail.com>
2017-09-23 16:49:37 -0300

    Cors adicionado na conf do spring security

--- src/main/java/academia/WebSecurityConfig.java
+++ src/main/java/academia/WebSecurityConfig.java
@@ -1,9 +1,7 @@
 package academia;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.csrf().disable();
+                http.cors().and().csrf().disable();
 		http.authorizeRequests().antMatchers("/**").hasRole("ADMIN").and().httpBasic();
 	}
 	

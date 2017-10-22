dd199b4a71599528d5ec8a788b60bdbe337b50be
Ayres Flesch <ayresabf@gmail.com>
2017-10-22 21:30:41 -0200

    Gerenciamento de matrícula

---
+++ src/main/java/academia/entidades/Matricula.java
@@ -0,0 +1,77 @@
+package academia.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Matricula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToOne
+    @JoinTable(name = "USER_MATRICULA")
+    private User usuario;
+
+    private String servicoContratado;
+
+    private String planoPagamento;
+
+    private String formaPagamento;
+
+    private String observacao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getServicoContratado() {
+        return servicoContratado;
+    }
+
+    public void setServicoContratado(String servicoContratado) {
+        this.servicoContratado = servicoContratado;
+    }
+
+    public String getPlanoPagamento() {
+        return planoPagamento;
+    }
+
+    public void setPlanoPagamento(String planoPagamento) {
+        this.planoPagamento = planoPagamento;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+}

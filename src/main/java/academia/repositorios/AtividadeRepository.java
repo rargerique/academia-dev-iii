6489642aa3dc17b37b0afb6a04e6457a5a9ff67c
Ayres Flesch <ayresabf@gmail.com>
2017-11-12 20:46:38 -0200

    Ajuste no AtividadeRepository

--- src/main/java/academia/repositorios/AtividadeRepository.java
+++ src/main/java/academia/repositorios/AtividadeRepository.java
@@ -1,18 +1,17 @@
 package academia.repositorios;
 
+import academia.entidades.Atividade;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import academia.entidades.Funcionario;
-
 @CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "atividades", path = "atividades")
-public interface AtividadeRepository {
+public interface AtividadeRepository extends PagingAndSortingRepository<Atividade, Long> {
 
-	@Query("SELECT n FROM Atividade n where n.descricao = ?1")
-    List<Funcionario> findByDescricao(@Param("descricao") String descricao);
+    @Query("SELECT n FROM Atividade n where n.descricao = ?1")
+    List<Atividade> findByDescricao(@Param("descricao") String descricao);
 }
